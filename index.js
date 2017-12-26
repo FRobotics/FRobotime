7cbb3640a9943da02f8bab1b3f746959d71f2e2f
asianboiFRC <mike.x.cao@gmail.com>
2017-12-26 18:08:43 -0500

    Working Code (Rewrite)

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', function (req, res) {
+	console.log(req.body);
+	res.sendFile(__dirname + "/success.html");
+});
+
+app.get('/frobotime.html', function (req, res) {
+	console.log(req.body);
+	res.sendFile(__dirname + "/frobotime.html");
+});
+
+app.get('/data/data.json', function (req, res) {
+	res.sendFile(__dirname + "/data/data.json");
+});
+
+app.listen(8080);
