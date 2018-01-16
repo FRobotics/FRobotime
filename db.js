c981c864bcd7c9e8fd4503a7cd88c193c8c3b876
Aceheliflyer <aceheliflyer@gmail.com>
2018-01-16 11:11:26 -0500

    Working code so far.

---
+++ db.js
@@ -0,0 +1,101 @@
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('./timetable.sqlite')
+const password = require('fs').readFileSync('./password.txt').toString().trim()
+var inProgress = false
+var time = null // eslint-disable-line no-unused-vars
+
+// Initialize
+exports.initialize = () => {
+  db.serialize(function () {
+    db.run(`
+            CREATE TABLE IF NOT EXISTS timetable (
+            workshopID VARCHAR(20), 
+            name VARCHAR(100), 
+            timestampStart VARCHAR(100), 
+            timestampEnd VARCHAR(100),
+            hours VARCHAR(100),
+            inProgress BOOLEAN)
+        `)
+  })
+
+  setInterval(() => {
+    if (new Date().getHours() === 2 && new Date().getMinutes() === 0) { this.endWorkshop() }
+  }, 60000)
+}
+
+exports.getWorkshopID = () => {
+  var year = String(new Date().getYear() + 1900)
+  var month = String(new Date().getMonth() + 1)
+  var day = String(new Date().getDate())
+  if (month.length < 2) { month = 0 + month }
+  if (day.length < 2) { day = 0 + day }
+  return year + month + day
+}
+
+exports.store = (data) => {
+  if (data.type === 'in') {
+    var workshopID = this.getWorkshopID()
+    db.run(`
+            INSERT OR IGNORE INTO timetable VALUES (
+            "${workshopID}",
+            "${data.name}",
+            "${new Date()}",
+            "0",
+            "0",
+            1)
+        `)
+    console.log('Data successfully stored!')
+  }
+}
+
+exports.workshopInProgress = () => {
+  return inProgress
+}
+
+exports.isOfficer = (pwd) => {
+  if (password === pwd) return true
+  else return false
+}
+
+exports.processWorkshop = (body) => {
+  return new Promise((resolve) => {
+    if (body.type === 'start') {
+      if (!inProgress) {
+        time = new Date()
+        inProgress = true
+        resolve(true)
+      } else {
+        resolve(false)
+      }
+    } else if (body.type === 'end') {
+      if (inProgress) {
+        this.endWorkshop()
+        resolve(true)
+      } else { resolve(false) }
+    }
+  })
+}
+
+exports.endWorkshop = () => {
+  if (inProgress) {
+    db.all(`SELECT * FROM timetable WHERE inProgress = ${1}`, function (err, rows) {
+      if (err) { console.log(err) } else if (!rows[0]) { console.log('nothing found ur an idiot') } else {
+        console.log(rows)
+      }
+    })
+    console.log('--- ENDED WORKSHOP ' + this.getWorkshopID() + '---')
+  }
+}
+
+exports.endWorkshop = () => {
+  if (inProgress) {
+    db.all(`SELECT * FROM timetable WHERE inProgress = ${1}`, function (err, rows) {
+      if (err) { console.log(err) } else if (!rows[0]) { console.log('nothing found ur an idiot') } else {
+        console.log(rows)
+        console.log('--- ENDED WORKSHOP ' + this.getWorkshopID() + '---')
+      }
+    })
+  }
+}
+
+exports.getStatus = () => { }
---
+++ index.js
@@ -0,0 +1,65 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+var db = require('./db.js')
+var childProcess = require('child_process')
+
+db.initialize()
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.set('views', './views')
+app.set('view engine', 'pug')
+
+app.post('/submit', function (req, res) {
+  console.log(req.body)
+  if (req.body.name && req.body.type) {
+    if (db.workshopInProgress()) {
+      console.log(req.body)
+      db.store(req.body)
+      res.render('success', { title: 'Hey', message: 'Hello there!' })
+    } else {
+      res.sendFile(__dirname + '/static/noworkshop.html') // eslint-disable-line no-path-concat
+    }
+  }
+})
+
+app.post('/workshop', function (req, res) {
+  console.log(req.body)
+  if (req.body.type && req.body.password) {
+    if (db.isOfficer(req.body.password)) {
+      db.processWorkshop(req.body).then(status => {
+        if (status && req.body.type === 'start') res.render('success', { title: 'Success', message: 'Workshop Started!' })
+        else if (status && req.body.type === 'end') res.render('success', { title: 'Success', message: 'Workshop Ended!' })
+        else if (req.body.type === 'start') res.send('There is already a workshop in progress.')
+        else if (req.body.type === 'end') res.sendFile(__dirname + '/static/noworkshop.html') // eslint-disable-line no-path-concat
+        else res.send('ERROR')
+      })
+    } else {
+      res.send('Incorrect Password!')
+    }
+  }
+})
+
+app.get('/update', function (req, res) {
+  var update = childProcess.execSync('git pull').toString()
+  console.log(update)
+  res.send(update)
+})
+
+/* eslint-disable no-path-concat */
+app.get('/restart', () => process.exit())
+app.get('/', (req, res) => res.sendFile(__dirname + '/frobotime.html'))
+app.get('/frobotime.html', (req, res) => res.sendFile(__dirname + '/frobotime.html'))
+app.get('/workshop.html', (req, res) => res.sendFile(__dirname + '/workshop.html'))
+app.get('/data/data.json', (req, res) => res.sendFile(__dirname + '/data/data.json'))
+app.get('/main.css', (req, res) => res.sendFile(__dirname + '/main.css'))
+app.get('/logo.jpg', (req, res) => res.sendFile(__dirname + '/logo.jpg'))
+
+app.use((req, res) => res.sendFile(__dirname + '/static/404.html'))
+/* eslint-enable no-path-concat */
+app.use('/', express.static('static'))
+
+app.listen(8080)
